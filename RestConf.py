9ff1b276e2e9a75b4045560e9ac318cd963a6f44
luvincanada <msminhas1@gmail.com>
2018-08-17 10:50:03 -0700

    Add files via upload

---
+++ RestConf.py
@@ -0,0 +1,127 @@
+import requests
+import os
+import json
+import time
+
+class RestCalls():
+
+    def __init__(self, ip_address, port=80, username=None, password=None):
+        self.BasePath = '/restconf/data/running/openconfig-'
+        #self.BasePath = ''
+        self.Accept = [
+            'application/yang.data+{fmt}',
+            'application/yang.errors+{fmt}',
+        ]
+        self.ContentType = 'application/yang.data+{fmt}'
+        session = requests.Session()
+        self.Format = 'json'
+        if username is not None and password is not None:
+            session.auth = (username, password)
+        session.verify = False
+        session.headers.update({
+            'Accept': ','.join([
+                accept.format(fmt=self.Format) for accept in self.Accept
+            ]),
+            'Content-Type': self.ContentType.format(fmt=self.Format),
+        })
+        self._session = session
+        self._host = '{scheme}://{ip}:{port}{basePath}'.format(
+            scheme='http',
+            ip=ip_address,
+            port=port,
+            basePath=self.BasePath
+        )
+        self.headers = {
+            'Content-Type': 'application/yang-data+json',
+        }
+        self.auth  = (username, password)
+
+    def put(self, data, endpoint):
+        url = self._host + endpoint
+        print(url)
+        res = requests.put(url, headers=self.headers, data=data, auth=self.auth)
+        return res
+
+    def post(self, data, endpoint):
+        url = self._host + endpoint
+        print(url)
+        res = requests.post(url, headers=self.headers, data=data, auth=self.auth)
+        return res
+
+    def patch(self, data, endpoint):
+        url = self._host + endpoint
+        print(data)
+        print(url)
+        res = requests.patch(url, headers=self.headers, data=data, auth=self.auth)
+        return res
+
+    def get(self, endpoint='', **kwargs):
+        url = self._host + endpoint
+        print(url)
+        if 'content' not in kwargs:
+            kwargs = {'content': 'config'}
+        print(url)
+        #res = self._session.get(url, params=kwargs)
+        res = requests.get(url, headers=self.headers, auth=self.auth)
+        return res
+
+    def delete(self, endpoint):
+        url = self._host + endpoint
+        print(url)
+        #res = self._session.delete(url)
+        res = requests.delete(url, auth=self.auth)
+        #res = requests.delete('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33', auth=('ADMIN', 'ADMIN'))
+        return res
+    
+def get_session():
+    ip = os.environ.get('SWITCH_IP')
+    print(ip)
+    port = os.environ.get('SWITCH_PORT')
+    print(port)
+    user = os.environ.get('REST_USER')
+    print(user)
+    pswd = os.environ.get('REST_PSWD')
+    print(pswd)
+    rest_session = RestCalls(ip,port,user,pswd)
+    return rest_session
+
+def get(uri=''):
+    rest_session = get_session()
+    resp = rest_session.get(uri)
+    return resp
+
+def delete(uri=''):
+    rest_session = get_session()
+    resp = rest_session.delete(uri)
+    #resp = requests.delete('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33', auth=('ADMIN', 'ADMIN'))
+    #time.sleep(1)
+    #data = '{"config":{"name":"lab-test-vlan33"},"vlan-id":33}'
+    #response = requests.delete('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33', auth=('ADMIN', 'ADMIN'))
+    return resp
+
+def post(uri='',data=None):
+    print(data)
+    rest_session = get_session()
+    resp = rest_session.post(data,uri)
+    return resp
+
+def put(uri='',data=None):
+    print(data)
+    rest_session = get_session()
+    resp = rest_session.put(data,uri)
+    return resp
+
+def patch(uri='',data=None):
+    print(data)
+    rest_session = get_session()
+    #resp = rest_session.patch(data,uri)
+    headers = {
+        'Content-Type': 'application/yang-data+json',
+    }
+    #data = '{"config":{"name":"lab-test-vlan33"},"vlan-id":33}'
+    #if 'vlan' in data:
+    #    resp = requests.patch('http://172.31.57.16:8538/restconf/data/running/openconfig-vlan:vlans/vlan=33/config/vlan-id', headers=headers, data=data, auth=('ADMIN', 'ADMIN'))
+    #else:
+    resp = rest_session.patch(data,uri)
+    return resp
+
---
+++ test.robot
@@ -0,0 +1,81 @@
+*** Settings ***
+Library       OperatingSystem
+Library       RestConf
+Library    Collections
+
+*** Variables ***
+${MESSAGE}    Hello, world!
+
+*** Test Cases ***
+Get Hostname
+    [Documentation]    Get host name of switch
+    [Tags]    get    hostname
+    ${result}=  get    system:system/config
+    Should Be Equal  ${result.status_code}  ${200}
+    ${json}=  Set Variable  ${result.json()}
+    Log    ${json}
+Put Hostname
+    [Documentation]    Change host name of switch
+    [Tags]    put    hostname
+    ${putresult}=  put    system:system/config    {"config":{"hostname":"Switch1"}}
+    Should Be Equal  ${putresult.status_code}  ${204}
+    ${result}=  get    system:system/config
+    Should Be Equal  ${result.status_code}  ${200}
+    ${json}=  Set Variable  ${result.json()}
+    Log    ${json}
+    json property should equal    ${json}    openconfig-system:system    {u'config':{u'hostname':u'Switch1'}}
+Patch Hostname
+    [Documentation]    Change host name of switch
+    [Tags]    put    hostname
+    ${putresult}=  patch    system:system/config    {"config":{"hostname":"Switch1"}}
+    Should Be Equal  ${putresult.status_code}  ${204}
+    ${result}=  get    system:system/config
+    Should Be Equal  ${result.status_code}  ${200}
+    ${json}=  Set Variable  ${result.json()}
+    Log    ${json}
+    json property should equal    ${json}    openconfig-system:system    {u'config': {u'hostname': u'Switch1'}}  
+Post Vlan
+    [Documentation]    Create vlan in switch
+    [Tags]    post    vlan
+    ${postresult}=    post    vlan:vlans    {"vlan-id":33}
+    Should Be Equal  ${postresult.status_code}  ${201}
+    ${result}=  get    vlan:vlans/vlan=33/config/vlan-id
+    Should Be Equal  ${result.status_code}  ${200}
+    ${json}=  Set Variable  ${result.json()}
+    Log    ${json}
+    json property should equal    ${json}    openconfig-vlan:vlans    {u'vlan': [{u'vlan-id': 33, u'config': {u'vlan-id': 33}}]}
+Patch Vlan
+    [Documentation]    Update vlan in switch
+    [Tags]    patch    vlan
+    ${patchresult}=    patch    vlan:vlans/vlan=33/config/vlan-id    {"config":{"name":"lab-test-vlan"},"vlan-id":33}
+    Should Be Equal  ${patchresult.status_code}  ${204}
+    ${result}=  get    vlan:vlans/vlan=33/config/vlan-id
+    Should Be Equal  ${result.status_code}  ${200}
+    ${json}=  Set Variable  ${result.json()}
+    Log    ${json}
+    json property should equal    ${json}    openconfig-vlan:vlans    {u'vlan': [{u'vlan-id': 33, u'config': {u'vlan-id': 33}}]}   
+Delete VLAN
+    [Documentation]    delete vlan in switch
+    [Tags]    delete    vlan
+    ${delresult}=    delete    vlan:vlans/vlan=33
+    Log    ${delresult.status_code}
+    Should Be Equal  ${delresult.status_code}  ${204}
+    ${result}=  get    vlan:vlans/vlan=33/config/vlan-id
+    Should Be Equal  ${result.status_code}  ${404}
+Post Log host
+    [Documentation]    Post Log Host
+    [Tags]    post    log
+    ${postresult}=    post    system:system/oc-log:logging/oc-log:remote-servers    {"oc-log:host":"192.189.7.6"}
+    Log    ${postresult.status_code}
+
+    
+*** Keywords ***
+Test Setup KW
+    delete    vlan:vlans/vlan=33
+    
+json property should equal    
+    [Arguments]  ${json}  ${property}  ${value_expected}
+    ${value_found} =    Get From Dictionary  ${json}  ${property}
+    ${error_message} =  Catenate  SEPARATOR=  Expected value for property "  ${property}  " was "  ${value_expected}  " but found "  ${value_found}  "
+    Should Be Equal As Strings  ${value_found}  ${value_expected}  ${error_message}    values=false
+
